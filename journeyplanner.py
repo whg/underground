48e59cea0151c2fda6aaa5e6c52b200502a39362
whg <wgallia@gmail.com>
2013-05-25 15:11:00 +0100

    added data

--- journeyplanner.py
+++ journeyplanner.py
@@ -1,74 +1,55 @@
 #!/usr/bin/env python
 
 import random
-
-#something to store station data
-class Station:
-	def __init__(self, name, lines=[], loc=(0,0)):
-		self.name = name
-		self.lines = lines
-		self.loc = loc
-
-class Journey:
-	def __init__(self, route=[], stops=[], changes=0, dist=0, sjns=[]):
-		#route: [start, (line, intermediate_station)*, line, end]
-		self.route = route
-		#stops: [start, station*, end]
-		self.stops = stops
-		#number of changes
-		self.changes = changes
-		#sum of as the crow flies distance between stops
-		self.dist = dist
-		#a simple ranking...
-		self.rank = len(stops) * (changes+1) * dist
-		#separate journeys
-		self.sjns = sjns
-# 		print "yo"
-
+import data
+from util import *
 
 class JourneyPlanner:
 	'''read files with data and then can plan a route for you
 	this is a slightly weird journey planner as i wanted to preserve
 	the lines used in a journey, so it's not truly a graph we working with
 	'''
-	
-	def __init__(self):			
-		self.lines = dict()
-		self.stations = dict()
-		self.loadstationdata()
-		self.shortestlen = 0
 
-	def loadstationdata(self):
-		#read the line files, and load the data
-		with open("./data/line_names", 'r') as f:	
-			line_names = [l.strip() for l in f.readlines()]
+	def __init__(self):
+		self.shortestlen = 0
+		
+	# def __init__(self):			
+	# 	self.lines = dict()
+	# 	self.stations = dict()
+	# 	self.loadstationdata()
+	# 	self.shortestlen = 0
+
+	# def loadstationdata(self):
+	# 	#read the line files, and load the data
+	# 	with open("./data/line_names", 'r') as f:	
+	# 		line_names = [l.strip() for l in f.readlines()]
 			
-			for line in line_names:
-				with open("./data/"+line, 'r') as g:
-					self.lines[line] = [l.strip() for l in g.readlines()]
+	# 		for line in line_names:
+	# 			with open("./data/"+line, 'r') as g:
+	# 				self.lines[line] = [l.strip() for l in g.readlines()]
 		
-		#work out which lines go through each station	
-		for l, sts in self.lines.iteritems():
-			for station in sts:
-				if station in self.stations:
-					self.stations[station].lines.append(l)
-				else:
-					self.stations[station] = Station(station, [l])
+	# 	#work out which lines go through each station	
+	# 	for l, sts in self.lines.iteritems():
+	# 		for station in sts:
+	# 			if station in self.stations:
+	# 				self.stations[station].lines.append(l)
+	# 			else:
+	# 				self.stations[station] = Station(station, [l])
 		
-		#add the positions to each station
-		with open("data/stations_position", 'r') as f:
-			for line in f:
-				st, p = line.split(':')
-				x,y,z = p.split(',')
-				if st in self.stations:
-					self.stations[st].loc = (float(x), float(y))
+	# 	#add the positions to each station
+	# 	with open("data/stations_position", 'r') as f:
+	# 		for line in f:
+	# 			st, p = line.split(':')
+	# 			x,y,z = p.split(',')
+	# 			if st in self.stations:
+	# 				self.stations[st].loc = (float(x), float(y))
 		
-	#this helper calculates distance between two stations
-	def dist(self, s1, s2):
-		x = (self.stations[s1].loc[0] - self.stations[s2].loc[0])**2
-		y = (self.stations[s1].loc[1] - self.stations[s2].loc[1])**2
-		#no need for expensive square root....
-		return (x+y)*1000 #mult by 1000 for easy reading
+	# #this helper calculates distance between two stations
+	# def dist(self, s1, s2):
+	# 	x = (self.stations[s1].loc[0] - self.stations[s2].loc[0])**2
+	# 	y = (self.stations[s1].loc[1] - self.stations[s2].loc[1])**2
+	# 	#no need for expensive square root....
+	# 	return (x+y)*1000 #mult by 1000 for easy reading
 	
 	
 	# FINDROUTE
@@ -78,12 +59,12 @@ class JourneyPlanner:
 		journey = journey + [start]
 		routes = []
 		
-		for line in self.stations[start].lines:
-			if end in self.lines[line]:
+		for line in data.stations[start].lines:
+			if end in data.lines[line]:
 				journey.extend([line, end])
 				return [journey]
 			
-			for station in self.lines[line]:
+			for station in data.lines[line]:
 				if station not in journey and line not in journey:
 					
 					#hack 1: max 2 changes
@@ -104,54 +85,11 @@ class JourneyPlanner:
 						
 		return routes
 	
-	def encodeline(self, line):
-		if line == "Bakerloo": return 0
-		elif line == "Central": return 1
-		elif line == "Circle": return 2
-		elif line == "District": return 3
-		elif line == "Hammersmith&City": return 4
-		elif line == "Jubilee": return 5
-		elif line == "Metropolitan": return 6
-		elif line == "Northern": return 7
-		elif line == "Piccadilly": return 8
-		elif line == "Victoria": return 9
-		elif line == "Waterloo&City": return 10
-
-	def decodeline(line):
-		if line == 0: return "Bakerloo"
-		elif line == 1: return "Central"
-		elif line == 2: return "Circle"
-		elif line == 3: return "District"
-		elif line == 4: return "Hammersmith&City"
-		elif line == 5: return "Jubilee"
-		elif line == 6: return "Metropolitan"
-		elif line == 7: return "Northern"
-		elif line == 8: return "Piccadilly"
-		elif line == 9: return "Victoria"
-		elif line == 10: return "Waterloo&City"
 	
 	def journey(self, source, dest):
 		
 		#find routes with duplicate lines
-		routeswd = self.findjourney(source, dest)	
-		
-# 		print routeswd
-# 		for i in routeswd: print len(i), i
-		
-		#now remove the duplicates, i.e:
-		#['Gloucester Road', 'District5', 'Earls Court', 'District3', 'Acton Town']
-		# routes = []
-		# for r in routeswd:
-		# 	lns = [l[0:-1] for l in r[1::2]]
-		# 	dup = False
-		# 	for l in lns:
-		# 		if lns.count(l) > 1:
-		# 			dup = True
-		#  			break
-		#  	if not dup:
-		#  		routes.append(r)
-		routes = routeswd
-				
+		routes = self.findjourney(source, dest)	
 		paths = []
 		
 		for path in routes:
@@ -175,21 +113,22 @@ class JourneyPlanner:
 				sjns = []
 				
 				for st, line in j:	
-					start = self.lines[line].index(st[0])
-					end = self.lines[line].index(st[1])
+					start = data.lines[line].index(st[0])
+					end = data.lines[line].index(st[1])
 					
 					#add the distance between each stop
 					for i in range(min([start, end]), max([start, end])-1):
-						d+= self.dist(self.lines[line][i], self.lines[line][i+1])
+						d+= data.dist(data.lines[line][i], data.lines[line][i+1])
 						
 					if (end - start) < 0:
-						t = self.lines[line][end:start+1]
+						t = data.lines[line][end:start+1]
 						t.reverse()
 					else:
-						t = self.lines[line][start:end+1]
+						t = data.lines[line][start:end+1]
 					
 					stops.extend(t)		
-					sjns.append((int("%i%s" % (self.encodeline(line[0:-1]), line[-1:])), t))
+					#sjns.append((int("%i%s" % (util.encodeline(line[0:-1]), line[-1:])), t))
+					sjns.append((line, t))
 					
 				#remove the number from the line
 				for i in range(len(lns)):
--- routefinder.py
+++ routefinder.py
@@ -1,64 +1,13 @@
 #!/usr/bin/env python
 	
 import math, random
+from util import Vector
 
 rsizes = [0 for i in range(50)]
 
 def print_end(): 
 	for i, e in enumerate(rsizes):
 		print i, e
-
-class Vector:
-	'''very simple vector class, doing just what we need'''
-	def __init__(self, x=0.0, y=0.0):
-		self.x = x
-		self.y = y
-			
-	def __repr__(self):
-		'''for pretty printing'''
-		return "(%.2f, %.2f)" % (self.x, self.y)
-	
-	def __sub__(self, v):
-		return Vector(self.x - v.x, self.y - v.y)
-		
-	def __add__(self, v):
-		return Vector(self.x + v.x, self.y + v.y)
-		
-	def __div__(self, s):
-		return Vector(self.x/s, self.y/s)
-	
-	def __mul__(self, s):
-		return Vector(self.x*s, self.y*s)
-		
-	def mag(self):
-		return math.sqrt(self.x**2 + self.y**2)
-	
-	def norm(self):
-		m = self.mag()
-		return Vector(self.x/m, self.y/m)
-	
-	def normalize(self):
-		'''this differs to norm(), as it normalises the vector in place'''
-		m = self.mag()
-		self.x/= m
-		self.y/= m
-		
-	def dot(self, v):
-		return self.x*v.x + self.y*v.y
-		
-	def angle(self, v): 
-		'''returns 5 it's going to try and divide by zero'''
-		if v.x == 0.0 and v.y == 0.0:
-			return 5
-		elif self.x == 0.0 and self.y == 0.0:
-			return 5
-		return math.acos(self.dot(v)/(self.mag()*v.mag()))
-		
-	def addnoise(self, sigma=0.5):
-		'''add gaussian noise to both components'''
-		self.x+= random.gauss(0, sigma)
-		self.y+= random.gauss(0, sigma)
-		return self
 
 class RouteFinder:
 	def __init__(self):
@@ -139,7 +88,7 @@ class RouteFinder:
 					result[i-1] = "%.2ff:%.2ff,%.2ff" % (point[1], point[0].x, point[0].y)
 			
 			try:
-				strres = "%i|%.1ff:%.1ff,%.1ff>%s" % (line, route[0][1], route[0][0].x, route[0][0].y, ";".join(result))
+				strres = "%i|%.1ff:%.1ff,%.1ff>%s" % (util.encodeline(line[:-1]), route[0][1], route[0][0].x, route[0][0].y, ";".join(result))
 				results.append(strres)
 			except Exception as e:
 				print 2, e
