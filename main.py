ddb68369736a70b9eb13d2680f421f354dbaa901
Will Gallia <wgallia@gmail.com>
2015-03-02 13:00:54 +0000

    not OO anymore, just functions in modules

--- main.py
+++ main.py
@@ -2,20 +2,20 @@
 
 import sys
 
-from journeyplanner import JourneyPlanner
-from routefinder import *
+from journeyplanner import journey
+from routefinder import findroute
 import time, sys
 from array import array
 from itertools import chain
 import data
 import cPickle
+import logging
+import os
+
+logging.basicConfig()
 
 beg = time.clock()
 
-jp = JourneyPlanner()
-rf = RouteFinder()
-
-# result = open("result"+sys.argv[1], 'w')
 p = True if len(sys.argv) > 2 else False
 p = False
 suf = '' if not len(sys.argv) > 2 else sys.argv[2]
@@ -23,55 +23,42 @@ done, i = 0, 0
 
 lists = [] #[[]]*8
 cols = []
-ses = []
+
+infospacing = 10
+outputdir = 'output'
 
 with open(sys.argv[1], 'r') as f:
     for line in f:
         tokens = line.split(',')
-        journeys =  jp.journey(tokens[3][1:-1], tokens[4][1:-1])
-        # print journeys[0][0][:-1]
-        route = rf.findroute(journeys, int(tokens[5][1:-1]), int(tokens[7][1:-1]))
+
+        journeys =  journey(tokens[3][1:-1], tokens[4][1:-1])
+        route = findroute(journeys, int(tokens[5][1:-1]), int(tokens[7][1:-1]))
+        
         if route:
-            # ses.extend(route)
             for line_col, j in route:
-                # pass
                 cols+= line_col
                 lists+= [j]
-                # result.write(r + "\n")
+
             if(p): print tokens[3][1:-1], "->", tokens[4][1:-1] #, '(%s)' % data.decodeline(int(r[0]))
             i+= 1
-        # else:
-            # print route, tokens[3][1:-1], tokens[4][1:-1]
-            # print line
         done+= 1
 
-        sys.stdout.write("\rdone: %d, failed: %d, fail rate: %2.2f%%" % (done, done-i, (done-i)/float(done)*100.0))
-        sys.stdout.flush()
+        if i % infospacing == 0:
+            sys.stdout.write("\rdone: %d, failed: %d, fail rate: %2.2f%%" % (done, done-i, (done-i)/float(done)*100.0))
+            sys.stdout.flush()
 
+print
 
-# cPickle.dump(ses, open('ses.pickle', 'wb'))
-# print 'ses:', ses
-
-# print_end()
-# print
-# print lists
-# print
+#write all the position data
 for i, z in enumerate(zip(*lists)):
-    with open('t%d%s.data' % (i, suf), 'wb') as f:
+    with open(os.path.join(outputdir, 't%d%s.data' % (i, suf)), 'wb') as f:
         a = array('f', chain(*z))
         a.tofile(f)
-    # print(zip(*lists))
 
-
-with open('cols.data%s' % suf, 'wb') as f:
+#write colour data
+with open(os.path.join(outputdir, 'cols.data%s' % suf), 'wb') as f:
     array('f', cols).tofile(f)
-    print len(cols)
 
-# sys.stdout.write("\n")
-# result.close()
-# with open("distr", 'w') as f:
-#     for i,j in enumerate(distr):
-#         f.write("%i,%i\n" % (i, j))
-    
-# print "completed in %.2fs" % ()
-print "-\ndone in %2.2f" % ((time.clock() - beg))
+print 'wrote a total of', len(cols), 'journeys'
+
+print "-\ncompleted in %2.2f" % ((time.clock() - beg))
--- routefinder.py
+++ routefinder.py
@@ -4,81 +4,79 @@ import data
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 
-class RouteFinder(object):
+#this returns the result in *string* format, not numbers...
+def findroute(journeys, stime, etime):
 
-   #this returns the result in *string* format, not numbers...
-    def findroute(self, journeys, stime, etime):
-      
-        total_time = etime - stime
-        if not journeys or total_time == 0:
-            logger.info('no journey')
-            return False
+    total_time = etime - stime
+    if not journeys or total_time == 0:
+        logger.info('no journey')
+        return False
 
 
-        total_dist = 0
-        for line, journey in journeys:
+    total_dist = 0
+    for line, journey in journeys:
 
-            start, end = data.getstartend(line, journey[0], journey[-1])
-            inc = 1 if (end > start) else -1
-            for i in range(start, end, inc):
-                s, e = i, i+inc #intermediate start, end
-                total_dist+= (data.positions[line][s] - data.positions[line][e]).mag()
-            
-        if total_dist == 0.0:
-            logger.info('total dist = 0')
-            return False
-            
-        speed = total_dist/total_time
+        start, end = data.getstartend(line, journey[0], journey[-1])
+        inc = 1 if (end > start) else -1
+        for i in range(start, end, inc):
+            s, e = i, i+inc #intermediate start, end
+            total_dist+= (data.positions[line][s] - data.positions[line][e]).mag()
 
-        
-        if speed > 30:
-            logger.info('massive speed!')
-            return False
-         
-        last, dist = Vector(), 0
-        linesandpoints = []
-      
-        for line, journey in journeys:
-         
-            start, end = data.getstartend(line, journey[0], journey[-1])
+    if total_dist == 0.0:
+        logger.info('total dist = 0')
+        return False
 
-            pos = data.positions[line][start]
-            p = [[pos.x, pos.y, dist/speed+stime]]
-            # p = [(data.positions[line][start], dist/speed+stime)]
-            inc = 1 if end > start else -1
-            for i in range(start, end, inc):
-                s, e = i, i+inc
-                direction = data.positions[line][s] - data.positions[line][e]
-                dist+= direction.mag()
-            
-            
-                #if the direction of the new direction is 
-                #almost the same as the old, replace the old with new
-                #try:
-                #print start_station, end_station, direction.angle(last)
-                pos = data.positions[line][e]
-                v = [pos.x, pos.y, dist/speed+stime]
+    speed = total_dist/total_time
 
-                if direction.angle(last) < 0.1 and len(p) > 1:
-                    p[-1] = v
-                else:
-                    p.append(v)
 
-                last = direction
+    # if speed > 30:
+    #     logger.info('massive speed!')
+    #     return False
 
-            linesandpoints.append((line, p))
-      
+    last, dist = Vector(), 0
+    linesandpoints = []
 
-        results = []
-      
-        for line, route in linesandpoints:
+    for line, journey in journeys:
 
-            if len(route) > 12:
-                continue
+        start, end = data.getstartend(line, journey[0], journey[-1])
 
-            results.append((data.line_colour(line[:-1]), route + [[0,0,0]]*(12-len(route))))
-      
-        return results
+        pos = data.positions[line][start]
+        p = [[pos.x, pos.y, dist/speed+stime]]
+        # p = [(data.positions[line][start], dist/speed+stime)]
+        inc = 1 if end > start else -1
+        for i in range(start, end, inc):
+            s, e = i, i+inc
+            direction = data.positions[line][s] - data.positions[line][e]
+            dist+= direction.mag()
+
+
+            #if the direction of the new direction is 
+            #almost the same as the old, replace the old with new
+            #try:
+            #print start_station, end_station, direction.angle(last)
+            pos = data.positions[line][e]
+            v = [pos.x, pos.y, dist/speed+stime]
+
+            if direction.angle(last) < 0.1 and len(p) > 1:
+                p[-1] = v
+            else:
+                p.append(v)
+
+            last = direction
+
+        linesandpoints.append((line, p))
+
+
+    results = []
+
+    for line, route in linesandpoints:
+
+        if len(route) > 12:
+            continue
+
+        results.append((data.line_colour(line[:-1]), route + [[0,0,0]]*(12-len(route))))
+
+    return results
